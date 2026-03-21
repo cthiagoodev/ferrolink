ac138978977afe90d3bd1d3966b84e59a8f75d5f
thiagoodev <cthiagoodev@gmail.com>
2026-03-20 23:15:00 -0300

    ferrolink server test

--- ferrolink-server/src/main.rs
+++ ferrolink-server/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:9000").await.unwrap();
+    println!("Listening on port 9000...");
+
+    loop {
+        let (socket, address) = listener.accept().await.unwrap();
+        println!("Accepted connection from: {address}");
+    }
 }
